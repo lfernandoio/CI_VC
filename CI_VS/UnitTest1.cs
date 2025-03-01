1abbbb56817abad9d4c967a11ae12ecc4b018c77
Fernando Ibarra <luisfernandoibarrao@gmail.com>
2025-03-01 12:39:21 -0400

    CI

---
+++ CI_VS/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace CI_VS
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(5,3);
+        }
+    }
+}
